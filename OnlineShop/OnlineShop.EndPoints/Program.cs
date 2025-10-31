c5ed0cc80081ce05d8ccd29a6d260810ddae44d9
nima bayat <nima.bt.1611@gmail.com>
2025-10-31 16:48:24 +0330

    book

--- OnlineShop/OnlineShop.EndPoints/Program.cs
+++ OnlineShop/OnlineShop.EndPoints/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Books}/{action=Report}/{id?}");
 
 app.Run();
